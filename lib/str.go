5720044d41b505a59e2a3f0e479a68eb16f08309
neo532 <neo532@126.com>
2020-11-10 11:38:47 +0800

    a converter for string

--- lib/str.go
+++ lib/str.go
@@ -58,3 +58,30 @@ func Float2str(num float64, decimal int) string {
 	}
 	return strconv.FormatFloat(math.Trunc(num*d)/d, 'f', -1, 64)
 }
+
+// String is a type of converter for string.
+type String string
+
+// Int returns the int after converting the string to int.
+func (s String) Int() int {
+	if i, err := strconv.Atoi(strings.Split(string(s), ".")[0]); nil == err {
+		return i
+	}
+	return 0
+}
+
+// Int64 returns the int64 after converting the string to int64.
+func (s String) Int64() int64 {
+	if i, err := strconv.ParseInt(strings.Split(string(s), ".")[0], 10, 64); nil == err {
+		return i
+	}
+	return 0
+}
+
+// Float64 returns the float64 after converting the string to float64.
+func (s String) Float64() float64 {
+	if i, err := strconv.ParseFloat(string(s), 64); nil == err {
+		return i
+	}
+	return 0
+}
