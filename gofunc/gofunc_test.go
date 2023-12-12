7051bbf3df7be2d3df3708a9f1cc32da064e6047
neo <neo532@126.com>
2023-12-12 17:18:04 +0800

    tidy test

--- gofunc/gofunc_test.go
+++ gofunc/gofunc_test.go
@@ -3,16 +3,16 @@ package gofunc
 import (
 	"context"
 	"fmt"
-	"runtime"
 	"testing"
 	"time"
 )
 
 func TestWithTimeout(t *testing.T) {
+
 	c, closeFn := context.WithCancel(context.Background())
+
 	fn := func(i int) (err error) {
 		time.Sleep(time.Second * 1)
-		fmt.Println(runtime.Caller(0))
 		fmt.Println(fmt.Sprintf("%s\t:Biz run,%d", t.Name(), i))
 		//err = errors.New("aaaaaaa")
 		return
@@ -21,26 +21,32 @@ func TestWithTimeout(t *testing.T) {
 	log := &DefaultLogger{}
 	gofn := NewGoFunc(WithLogger(log), WithMaxGoroutine(20))
 
-	//gofn.Go(c, fn, fn, fn)
+	l := 3
+	fns := make([]func(i int) error, 0, l)
+	for i := 0; i < l; i++ {
+		fns = append(fns, fn)
+	}
+
+	gofn.Go(c, fns...)
 
 	go func() {
 		go func() {
 			gofn.WithTimeout(
 				c,
 				time.Second*5,
-				fn,
-				fn,
+				fns...,
 			)
-			fmt.Println(fmt.Sprintf("print<<<\t%+v>>>", log.Err()))
+			err := log.Err()
+			fmt.Println(fmt.Sprintf("print<<<\t%+v>>>", err))
 		}()
 		select {
 		case <-c.Done():
 			return
 		}
-		fmt.Println(runtime.Caller(0))
+		fmt.Println("Exec Finish")
 	}()
 
 	time.Sleep(4 * time.Second)
-	fmt.Println(runtime.Caller(0))
+	fmt.Println("End")
 	closeFn()
 }
